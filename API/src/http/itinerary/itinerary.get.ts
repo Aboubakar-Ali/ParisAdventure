0b04448067b2be1b051f704e8fb4f86a81270a7f
Luxchar <luccharlopeau@gmail.com>
2023-11-04 18:33:13 +0100

    api sorting

--- API/src/http/itinerary/itinerary.get.ts
+++ API/src/http/itinerary/itinerary.get.ts
@@ -9,31 +9,65 @@ export default {
     method: "POST",
     run: async (req: express.Request, res: express.Response) => {
         try {
-            const {adults, hours, position, price} = req.body
-            
-            if (!position || !hours || !price || !adults) throw "Badly formatted"
+            const { adults, hours, position, price } = req.body;
 
-            console.log(position, hours, price, adults)
+            if (!position || !hours || !price || !adults) throw "Badly formatted";
+
+            console.log(position, hours, price, adults);
             // get all activities, restaurants from database
-            const activities = await Activities.find({})
-            const restaurants = await Restaurants.find({})
-            // calculate the best itinerary
+            const activities = await Activities.find({});
+            const restaurants = await Restaurants.find({});
 
-            // console.log(activities)
-            console.log(restaurants)
-            // return the best itinerary
+            if (!activities || !restaurants) throw "No activities or restaurants found";
 
-            
+            // SORTING
+            const bestItinerary: Array<typeof Activities | typeof Restaurants> = [];
 
-            res.status(200)
-            res.send("Test route response");
+            // sort activities and restaurants by distance (position)
+            // TODO
 
+            //sort activities and restaurants by rating (note)
+            activities.sort((a: Activities, b: Activities) => {
+                return b.note - a.note;
+            });
 
-        }
+            // restaurants.sort((a: Restaurants, b: Restaurants) => {
+            //     return b.note - a.note;
+            // });
 
-        catch(err) {
-            res.status(400)
+            // for each activity, check if it fits the user's preferences (hours, price)
+            for (const activity of activities) {
+                // Check if the activity fits the user's preferences
+                if (activity.time > hours || activity.price > price) {
+                    continue;
+                }
+
+                // Check if the activity is already in the itinerary
+                if (bestItinerary.includes(activity)) {
+                    continue;
+                }
+
+                // Add the activity to the itinerary
+                bestItinerary.push(activity);
+
+                // Calculate the remaining hours and price
+                hours -= activity.time;
+                price -= activity.price;
+
+                // Break if the user has no more hours or price remaining
+                if (hours <= 0 || price <= 0) {
+                    break;
+                }
+            }
+
+            if (!bestItinerary) throw "No best itinerary found";
+
+            Logger.info(`Best itinerary found: ${bestItinerary}`);
+            res.status(200);
+            res.send(bestItinerary);
+        } catch (err) {
+            res.status(400);
             res.send("Test route response");
         }
     }
-}
+};

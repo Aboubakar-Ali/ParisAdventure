053d8bd7c445d43f85875c7d6ddda404aa5a464e
Theo <118967935+FalcoBo@users.noreply.github.com>
2023-11-06 10:47:48 +0100

    fix geocode

--- WebApp/js/coordonees.js
+++ WebApp/js/coordonees.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const opencage = require('opencage-api-client');
+const axios = require('axios');
 
 const files_csv = ['./Scrapping/restaurants/Clean.csv', './Scrapping/activities/Clean.csv'];
-const outputFilePath = 'resultats_geocodage.csv'; // Spécifiez le chemin du nouveau fichier de résultats
+const outputFilePath = 'resultats_geocodage.csv';
 const addresses = [];
 const geocodingPromises = [];
 
@@ -12,9 +12,13 @@ const apiKey = '6748e70e5b8344639705ca7125622a68';
 
 // Fonction pour géocoder une adresse
 function geocodeAddress(address) {
+  const encodedAddress = encodeURIComponent(address);
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodedAddress}&key=${apiKey}`;
+
   return new Promise((resolve, reject) => {
-    opencage.geocode({ q: address, key: apiKey })
-      .then(data => {
+    axios.get(url)
+      .then(response => {
+        const data = response.data;
         if (data.results.length > 0) {
           const result = data.results[0];
           const coordinates = {
@@ -23,7 +27,8 @@ function geocodeAddress(address) {
           };
           resolve(coordinates);
         } else {
-          reject('Adresse non trouvée pour : ' + address); // Ajout de l'adresse à l'erreur
+          // Adresse non trouvée, résout avec null
+          resolve(null);
         }
       })
       .catch(error => {
@@ -37,9 +42,8 @@ files_csv.forEach((file) => {
   fs.createReadStream(file)
     .pipe(csv())
     .on('data', (row) => {
-      const address = row.address; // Utilisez directement la valeur de l'adresse
+      const address = row.address;
       if (address && address.trim() !== '') {
-        // console.log('Adresse avant géocodage :', address);
         addresses.push(address);
         geocodingPromises.push(geocodeAddress(address));
       }
@@ -48,16 +52,15 @@ files_csv.forEach((file) => {
 
 Promise.all(geocodingPromises)
   .then((coordinatesArray) => {
-    console.log(coordinatesArray);
+    console.log('Toutes les géocodes :', coordinatesArray);
 
-    // Créez un tableau de résultats contenant les adresses et les géocodes correspondants
-    const results = addresses.map((address, index) => ({
-      Address: address,
-      Geocode: coordinatesArray[index],
-    }));
-
-    results.forEach((result) => {
-      console.log(`Adresse géocodée : ${result.Address}, Geocode : ${JSON.stringify(result.Geocode)}`);
+    // Créez un fichier CSV avec les adresses et les géocodes correspondants
+    const records = addresses.map((address, index) => {
+      const geocode = coordinatesArray[index] ? coordinatesArray[index] : 'Non géocodé';
+      return {
+        Address: address,
+        Geocode: JSON.stringify(geocode),
+      };
     });
 
     // Créez un nouveau fichier CSV avec les résultats
@@ -69,9 +72,9 @@ Promise.all(geocodingPromises)
       ],
     });
 
-    csvWriter.writeRecords(results)
+    csvWriter.writeRecords(records)
       .then(() => {
-        console.log('Résultats de géocodage écrits dans le fichier CSV :', results);
+        console.log('Résultats de géocodage écrits dans le fichier CSV :', records);
         console.log(`Résultats de géocodage écrits dans ${outputFilePath}`);
       })
       .catch((error) => {
---
+++ geocode.py
@@ -0,0 +1,55 @@
+import csv
+import requests
+import json
+
+# Liste des fichiers CSV à traiter
+files_csv = ['./Scrapping/restaurants/Clean.csv', './Scrapping/activities/Clean.csv']
+
+# Clé d'API OpenCage Data
+api_key = '6748e70e5b8344639705ca7125622a68'
+
+# Chemin du fichier de sortie
+output_file = 'resultats_geocodage.csv'
+
+addresses = []
+geocoding_results = []
+
+# Fonction pour géocoder une adresse
+def geocode_address(address):
+    encoded_address = address.replace(" ", "+")
+    url = f'https://api.opencagedata.com/geocode/v1/json?q={encoded_address}&key={api_key}'
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        if 'results' in data and len(data['results']) > 0:
+            result = data['results'][0]
+            coordinates = result['geometry']
+            return coordinates
+        else:
+            return None
+    except Exception as e:
+        return str(e)
+
+# Parcourir les fichiers CSV et collecter les adresses
+for file in files_csv:
+    with open(file, 'r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            address = row['address']
+            if address.strip():
+                addresses.append(address)
+                geocoding_results.append(geocode_address(address))
+
+# Créer un fichier CSV avec les résultats
+with open(output_file, 'w', newline='') as csv_file:
+    fieldnames = ['Address', 'Geocode']
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    
+    for i in range(len(addresses)):
+        address = addresses[i]
+        geocode = geocoding_results[i]
+        csv_writer.writerow({'Address': address, 'Geocode': json.dumps(geocode) if geocode else 'Non géocodé'})
+
+print('Résultats de géocodage écrits dans le fichier CSV :', output_file)

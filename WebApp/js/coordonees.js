55e26d58c8e5048dde1da332a839e42431c286c6
Theo <118967935+FalcoBo@users.noreply.github.com>
2023-11-06 10:19:31 +0100

    coord

--- WebApp/js/coordonees.js
+++ WebApp/js/coordonees.js
@@ -1,34 +1,89 @@
 const fs = require('fs');
 const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const opencage = require('opencage-api-client');
 
-// const apiKey = 'YOUR_API_KEY';
+const files_csv = ['./Scrapping/restaurants/Clean.csv', './Scrapping/activities/Clean.csv'];
+const outputFilePath = 'resultats_geocodage.csv'; // Spécifiez le chemin du nouveau fichier de résultats
 const addresses = [];
+const geocodingPromises = [];
 
-const files_csv = ['./Scrapping/activities/activities.csv','./Scrapping/hotels/hotels.csv','./Scrapping/restaurants/restaurants.csv','./Scrapping/restaurants/Clean.csv','./Scrapping/activities/Clean.csv'];
+const apiKey = '6748e70e5b8344639705ca7125622a68';
 
-fs.createReadStream(files_csv) // Remplacez 'votre_fichier.csv' par le chemin de votre fichier CSVs
-  .pipe(csv())
-  .on('data', (row) => {
-    const address = [row.address,row.ADRESSE,row.adress,row.Adress];
-    if (address) {
-      addresses.push(address);
-    }
-  })
-  .on('end', () => {
-    console.log('Adresses extraites :', addresses);
+// Fonction pour géocoder une adresse
+function geocodeAddress(address) {
+  return new Promise((resolve, reject) => {
+    opencage.geocode({ q: address, key: apiKey })
+      .then(data => {
+        if (data.results.length > 0) {
+          const result = data.results[0];
+          const coordinates = {
+            lat: result.geometry.lat,
+            lng: result.geometry.lng
+          };
+          resolve(coordinates);
+        } else {
+          reject('Adresse non trouvée pour : ' + address); // Ajout de l'adresse à l'erreur
+        }
+      })
+      .catch(error => {
+        reject(error);
+      });
   });
+}
 
+// Lisez tous les fichiers CSV et collectez les adresses et les promesses de géocodage
+files_csv.forEach((file) => {
+  fs.createReadStream(file)
+    .pipe(csv())
+    .on('data', (row) => {
+      const address = row.address; // Utilisez directement la valeur de l'adresse
+      if (address && address.trim() !== '') {
+        // console.log('Adresse avant géocodage :', address);
+        addresses.push(address);
+        geocodingPromises.push(geocodeAddress(address));
+      }
+    });
+});
 
-Promise.all(addresses.map(geocodeAddress))
-  .then(coordinatesArray => {
+Promise.all(geocodingPromises)
+  .then((coordinatesArray) => {
     console.log(coordinatesArray);
+
+    // Créez un tableau de résultats contenant les adresses et les géocodes correspondants
+    const results = addresses.map((address, index) => ({
+      Address: address,
+      Geocode: coordinatesArray[index],
+    }));
+
+    results.forEach((result) => {
+      console.log(`Adresse géocodée : ${result.Address}, Geocode : ${JSON.stringify(result.Geocode)}`);
+    });
+
+    // Créez un nouveau fichier CSV avec les résultats
+    const csvWriter = createCsvWriter({
+      path: outputFilePath,
+      header: [
+        { id: 'Address', title: 'Address' },
+        { id: 'Geocode', title: 'Geocode' },
+      ],
+    });
+
+    csvWriter.writeRecords(results)
+      .then(() => {
+        console.log('Résultats de géocodage écrits dans le fichier CSV :', results);
+        console.log(`Résultats de géocodage écrits dans ${outputFilePath}`);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de l\'écriture du fichier CSV :', error);
+      });
   })
-  .catch(error => {
-    console.error('Une erreur s\'est produite lors de la géocodage :', error);
+  .catch((error) => {
+    console.error('Une erreur s\'est produite lors du géocodage :', error);
   });
+
 
 // const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
-
 // // Effectuer une requête HTTP pour récupérer les données de géocodage
 // fetch(geocodeUrl)
 //   .then(response => response.json())

56cc139e531d14f2a06aa12c7043787b7d20d02d
Theo <118967935+FalcoBo@users.noreply.github.com>
2023-11-07 09:12:32 +0100

    fix filter geocode

--- Scrapping/geo/filter_csv.py
+++ Scrapping/geo/filter_csv.py
@@ -9,6 +9,9 @@ df = pd.read_csv(input_file, encoding='utf-8')
 # Filtrer les lignes qui continennent les valeurs suivantes
 df = df[~df['Geocode'].str.contains('Non g�ocod�')]
 df = df[~df['Address'].str.contains('Tout afficher')]
+df = df[~df['Address'].str.startswith('Appeler')]
+df = df[~df['Address'].str.startswith('En savoir plus')]
+
 
 # Sauvegarder le résultat dans un nouveau fichier CSV
 df.to_csv(output_file, index=False, encoding='utf-8')
